3c936d20b7098d9ec468ceecc4e35e9db1344c46
prashant739 <87006627+prashant739@users.noreply.github.com>
2021-07-06 13:23:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,286 @@
+const music = [{
+    musicName: "Saza E Maut",
+    artistName: "Raftaar x KR$NA",
+    musicSrc: "https://jatt.download/music/data/Punjabi/202103/Still_Here/128/Saza_E_Maut_1.mp3",
+    musicPoster: "https://i.ytimg.com/vi/o907r6NsK9s/maxresdefault.jpg",
+    liked: true,
+  },
+  {
+    musicName: "Viah di Khabar",
+    artistName: "Kaka",
+    musicSrc: "https://jatt.download/music/data/Single_Track/202106/Viah_Di_Khabar/128/Viah_Di_Khabar_1.mp3",
+    musicPoster: "https://a10.gaanacdn.com/gn_img/albums/d41WjznWPL/1WjzXn6AWP/size_xxl.jpg",
+    liked: false,
+  },
+  {
+    musicName: "Tu Phir Se Aana",
+    artistName: "Raftaar x Salim Merchant x Karma",
+    musicSrc: "https://medium.cdnmp3.com/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhbGJ1bV9pZCI6NDUwMjMsInRpbWVvdXQiOjE2MjU1NTU5MDl9.vIBz3gqioiZc7BHcu9YfT_-bta_NXCD9K6727Q9O1oc/gvmyh/Tu%20Phir%20Se%20Aana%20-%20%28amlijatt.in%29.mp3",
+    musicPoster: "https://1.bp.blogspot.com/-kX21dGUuTdM/X85ij1SBeEI/AAAAAAAAKK4/feboCtDKkls19cZw3glZWRdJ6J8alCm-gCNcBGAsYHQ/s16000/Tu%2BAana%2BPhir%2BSe%2BRap%2BSong%2BLyrics%2BBy%2BRaftaar.jpg",
+  },
+  {
+    musicName: "Naachne Ka Shaunq",
+    artistName: "Raftaar x Brobha V",
+    musicSrc: "https://mp3.filmisongs.com/Naachne%20Ka%20Shaunq%20Mp3%20Song%20By%20Raftaar%20Ft%20Brodha%20V.mp3",
+    musicPoster: "https://i.ytimg.com/vi/QvswgfLDuPg/maxresdefault.jpg",
+  },
+  {
+    musicName: "Mantoiyat",
+    artistName: "Raftaar x Nawazuddin Siddiqui",
+    musicSrc: "http://dl2.djring.com/sd2.djring.com/128/495823/(DJJOhAL.Com).mp3",
+    musicPoster: "https://a10.gaanacdn.com/images/song/39/24225939/crop_480x480_1536749130.jpg",
+  },
+  {
+    musicName: "I'm ready",
+    artistName: "Raftaar x KR$NA",
+    musicSrc: "http://dl2.djring.com/sd2.djring.com/128/486263/(DJJOhAL.Com).mp3",
+    musicPoster: "https://a10.gaanacdn.com/images/albums/90/2009690/crop_480x480_2009690.jpg",
+  },
+  {
+    musicName: "Microphone Check",
+    artistName: "Raftaar",
+    musicSrc: "https://medium.cdnmp3.com/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhbGJ1bV9pZCI6NDQ3NjMsInRpbWVvdXQiOjE2MjU1NTY1NDV9.JV7WYDGaeAdFmoxn3ACpddUTTxGTtXq6ppjG4vrYib4/ummyh/Microphone%20Check%20-%20%28amlijatt.in%29.mp3",
+    musicPoster: "https://1.bp.blogspot.com/-3xC8UxACBBs/X7O5YLd5Y0I/AAAAAAAAEdc/Jhf3_uWkhBQH3grgqD878SHr-Aiz4L-uACLcBGAsYHQ/w640/microphone%2Bcheck.webp",
+  },
+  {
+    musicName: "BlackSheep",
+    artistName: "Raftaar",
+    musicSrc: "https://medium.cdnmp3.com/eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJhbGJ1bV9pZCI6NDU5MTMsInRpbWVvdXQiOjE2MjU1NTYxODJ9.OBg3kfsVDzAYO9gLcS6SohGjLLreqkiORrhLQ7aOkDU/zmcyh/Black%20Sheep%20-%20%28amlijatt.in%29.mp3",
+    musicPoster: "http://a10.gaanacdn.com/images/albums/31/3840631/crop_480x480_3840631.jpg",
+  },
+  {
+    musicName: "Saath Ya Khilaaf ",
+    artistName: "Raftaar x KR$NA",
+    musicSrc: "https://pwdown.com/113462/Saath%20Ya%20Khilaaf%20-%20Raftaar.mp3",
+    musicPoster: "https://1.bp.blogspot.com/-nIkGlZdgqII/X3PvZQk5VYI/AAAAAAAADrQ/Rx7lCvdcuGY4Q2PnyMwJKYVVz9rxXEE3wCLcBGAsYHQ/w640/saath%2Bya%2Bkhilaaf.jpg",
+  },
+  {
+    musicName: "Dilli Waali Baatcheet",
+    artistName: "Raftaar",
+    musicSrc: "https://pagalsong.in/uploads/systemuploads/mp3/Mr%20Nair/Dilli%20Waali%20Baatcheet%20-%20Mr%20Nair%20128%20Kbps.mp3",
+    musicPoster: "https://a10.gaanacdn.com/images/song/49/28009549/crop_480x480_1566383860.jpg",
+  },
+  {
+    musicName: "Damn",
+    artistName: "Raftaar x kr$na",
+    musicSrc: "https://mp3land.co/try/browse.php?u=https://pwdown.com/113462/Damn%20-%20Raftaar.mp3",
+    musicPoster: "https://filmisongs.xyz/wp-content/uploads/2020/07/Damn-Song-Raftaar-KrNa.jpg",
+  },
+  {
+    musicName: "Feeling You",
+    artistName: "Raftaar x Harjas",
+    musicSrc: "https://pagalfree.com/musics/128-Feeling%20You%20-%20Mr.Nair%20128%20Kbps.mp3",
+    musicPoster: "https://a10.gaanacdn.com/gn_img/albums/YoEWlabzXB/oEWlj5gYKz/size_xxl_1586752323.webp",
+  },
+];
+
+
+let musicIndex = 0;
+const audio = document.querySelector("#audio");
+const muteBtn = document.querySelector("#mute-btn");
+const prevBtn = document.querySelector("#backward-btn");
+const playBtn = document.querySelector("#play-pause-btn");
+const nextBtn = document.querySelector("#forward-btn");
+const musicName = document.querySelector("#song-name");
+const artistName = document.querySelector("#artist-name");
+const musicPoster = document.querySelector(".music-poster");
+const musicPosterBoxContainer = document.querySelector(".music-poster-box-container");
+
+let isPlaying = false;
+
+function currentMusic(index) {
+  if (index >= music.length) {
+    index = index % music.length;
+  }
+  if (index < 0) {
+    index = music.length + index % music.length;
+  }
+  musicIndex = index;
+  musicPosterBoxContainer.innerHTML = "";
+  musicPosterBoxContainer.style.left = 0 + "%";
+  musicPosterBoxContainer.style.left = -250 * index + "px";
+  audio.src = music[index].musicSrc;
+  musicName.innerText = music[index].musicName;
+  artistName.innerText = music[index].artistName;
+  musicPoster.src = music[index].musicPoster;
+  musicPoster.alt = music[index].musicName;
+  let list = document.querySelector(".list");
+  list.innerHTML = "";
+  music.forEach((item, index) => {
+    let musicPosterBox = document.createElement("img");
+    musicPosterBox.classList.add("music-poster-box");
+    musicPosterBox.src = music[index].musicPoster;
+    musicPosterBoxContainer.appendChild(musicPosterBox);
+    if (index == musicIndex) {
+      musicPosterBox.classList.add("current");
+    } else {
+      musicPosterBox.classList.remove("current");
+    }
+    //music list
+    let musicItem = document.createElement("div");
+    musicItem.classList.add("music");
+    //series
+    let serise = document.createElement("span");
+    serise.classList.add("series");
+    serise.innerText = index + 1 + ".";
+    musicItem.appendChild(serise);
+    //name
+    let name = document.createElement("span");
+    name.classList.add("music-name");
+    name.innerText = music[index].musicName;
+    musicItem.appendChild(name);
+    //bars
+    const bars = "<span class='bar n1'></span><span class='bar n2'></span><span class='bar n3'></span>";
+    let playing = document.createElement("div");
+    playing.classList.add("now");
+    if (index == musicIndex) {
+      playing.classList.add("playing");
+    } else {
+      playing.classList.remove("playing");
+    }
+    playing.innerHTML = bars;
+    musicItem.appendChild(playing);
+    list.appendChild(musicItem);
+  });
+}
+
+muteBtn.addEventListener('click', () => {
+    muteMusic();
+});
+
+playBtn.addEventListener('click', () => {
+  isPlaying ? pauseMusic() : playMusic();
+});
+
+nextBtn.addEventListener("click", () => {
+  musicIndex = musicIndex + 1;
+  currentMusic(musicIndex);
+  playMusic();
+});
+
+prevBtn.addEventListener("click", () => {
+  musicIndex = musicIndex - 1;
+  currentMusic(musicIndex);
+  playMusic();
+});
+
+currentMusic(musicIndex);
+
+function muteMusic() {
+  if (audio.muted == false)
+    audio.muted = true;
+  else
+    audio.muted = false;
+}
+
+function playMusic() {
+  isPlaying = true;
+  audio.play();
+  playBtn.classList.replace("fa-play-circle", "fa-pause-circle-o");
+}
+
+function pauseMusic() {
+  isPlaying = false;
+  audio.pause();
+  playBtn.classList.replace("fa-pause-circle-o", "fa-play-circle");
+}
+
+// music time
+const current = document.querySelector("#music-current");
+const duration = document.querySelector("#music-duration");
+if (duration.innerText == "NaN:0NaN") {
+  duration.innerText = "";
+}
+
+audio.addEventListener("timeupdate", () => {
+  let time = audio.currentTime;
+  let seconds = Math.floor(time % 60);
+  let minutes = Math.floor(time / 60);
+  if (minutes < 10 && seconds < 10) {
+    current.innerText = "0" + minutes + ":0" + seconds;
+  } else if (minutes < 10) {
+    current.innerText = "0" + minutes + ":" + seconds;
+  } else if (seconds < 10) {
+    current.innerText = minutes + ":0" + seconds;
+  } else {
+    current.innerText = minutes + ":0" + seconds;
+  }
+
+  //duration time
+  time = audio.duration;
+  seconds = Math.floor(time % 60);
+  minutes = Math.floor(time / 60);
+  if (minutes < 10 && seconds < 10) {
+    duration.innerText = "0" + minutes + ":0" + seconds;
+  } else if (minutes < 10) {
+    duration.innerText = "0" + minutes + ":" + seconds;
+  } else if (seconds < 10) {
+    duration.innerText = minutes + ":0" + seconds;
+  } else if (minutes >= 0 && seconds >= 0) {
+    duration.innerText = minutes + ":" + seconds;
+  } else {
+    duration.innerText = "00:00";
+  }
+
+  // time bar
+  const timebarCicle = document.querySelector(".timebar-circle");
+  const timebar = document.querySelector(".timebar");
+
+  timebar.style.width = (audio.currentTime / audio.duration) * 100 + "%";
+  timebarCicle.style.left = (audio.currentTime / audio.duration) * 100 + "%";
+
+
+  if (audio.currentTime == audio.duration) {
+    musicIndex = musicIndex + 1;
+    currentMusic(musicIndex);
+    playMusic();
+  }
+
+});
+
+const musicTimebar = document.querySelector(".music-timebar");
+musicTimebar.addEventListener("click", (e) => {
+  let a = e.offsetX;
+  const b = e.srcElement.clientWidth;
+  audio.currentTime = a / b * audio.duration;
+});
+
+
+
+//music list open
+const menuBtn = document.querySelector(".menu-btn");
+const musicList = document.querySelector(".music-list");
+const closeBtn = document.querySelector(".close-btn");
+
+menuBtn.addEventListener("click", () => {
+  musicList.classList.add("open");
+});
+
+closeBtn.addEventListener("click", () => {
+  musicList.classList.remove("open");
+});
+
+
+
+// swap left right
+
+const playerCenter = document.querySelector(".poster");
+let manager = new Hammer.Manager(playerCenter);
+
+// Create a recognizer
+let Swipe = new Hammer.Swipe();
+
+manager.add(Swipe);
+
+manager.on('swipe', function(e) {
+  let direction = e.offsetDirection;
+  if (direction == 4) {
+    musicIndex = musicIndex - 1;
+    currentMusic(musicIndex);
+    playMusic();
+  } else if (direction == 2) {
+    musicIndex = musicIndex + 1;
+    currentMusic(musicIndex);
+    playMusic();
+  }
+});
